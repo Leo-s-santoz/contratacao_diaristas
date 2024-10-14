610d6e0b68a03a2abfd7419daf36c0b0752862f3
Leo-s-santoz <leonardosantos11080@gmail.com>
2024-10-14 17:08:55 -0300

    add - paginas de registro e login /// frontend pagina de login completa

---
+++ index.js
@@ -0,0 +1,66 @@
+function onChangeEmail() {
+  toggleButtonsDisable();
+  toggleEmailError();
+}
+
+function onChangePassword() {
+  toggleButtonsDisable();
+  togglePasswordError();
+}
+
+function isEmailValid() {
+  const email = form.email().value;
+  if (!email) {
+    return false;
+  }
+  return validateEmail(email);
+}
+
+function toggleEmailError() {
+  const email = form.email().value;
+  (form.EmailRequiredError().style.display = email ? "none" : "block"),
+    (form.EmailInvalidError().style.display = validateEmail(email)
+      ? "none"
+      : "block");
+}
+
+function togglePasswordError() {
+  const password = form.password().value;
+  form.PasswordRequiredError().style.display = password ? "none" : "block";
+}
+
+function toggleButtonsDisable() {
+  const emailValid = isEmailValid();
+  form.RecoverPasswordButton().disabled = !emailValid;
+
+  const passwordValid = isPasswordValid();
+  form.LoginButton().disabled = !emailValid || !passwordValid;
+}
+
+function isPasswordValid() {
+  const password = form.password().value;
+  return !!form.password().value;
+}
+
+function validateEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
+
+function login() {
+  window.location.href = "/pages/home/home.html";
+}
+
+function register() {
+  window.location.href = "/pages/register/register.html";
+}
+
+const form = {
+  email: () => document.getElementById("email"),
+  password: () => document.getElementById("password"),
+  EmailRequiredError: () => document.getElementById("EmailRequiredError"),
+  EmailInvalidError: () => document.getElementById("EmailInvalidError"),
+  PasswordRequiredError: () => document.getElementById("PasswordRequiredError"),
+  RecoverPasswordButton: () => document.getElementById("RecoverPasswordButton"),
+  LoginButton: () => document.getElementById("LoginButton"),
+};
